3e095234fc812c94477702b00c34ecf8ed0d92cb
Jack <jacktasia@gmail.com>
2015-12-02 10:35:11 -0800

    add fake data for tests

--- test/dumb-jump-test.el
+++ test/dumb-jump-test.el
@@ -1,5 +1,11 @@
 (require 'dumb-jump)
 (require 'ert)
+(require 'f)
+
+(setq test-data-dir (f-expand "./test/data"))
 
 (ert-deftest dumb-jump-dummy-test ()
   (should (string= (dumb-jump-asdf) "asdf")))
+
+(ert-deftest data-dir-exists-test ()
+  (should (f-dir? test-data-dir)))
