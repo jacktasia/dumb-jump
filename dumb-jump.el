84c0995086d4417ea2850bf388b0323ce412d87c
Jack <jacktasia@gmail.com>
2015-12-14 18:48:12 -0800

    core function for grabbing languages,modes, rules

--- dumb-jump.el
+++ dumb-jump.el
@@ -13,13 +13,63 @@
 ;; (require 'json)
 ;; (require 'url)
 ;; (require 'ht)
-;; (require 'dash)
 ;; (require 's)
 ;; (require 'pp)
 ;; (require 'cl-lib)
+(require 'dash)
 
 (defun dumb-jump-asdf ()
   "asdf")
+
+;; TODO: document defvars
+(defvar dumb-jump-grep-prefix "LANG=C grep")
+
+(defvar dumb-jump-grep-args "-REn")
+
+(defvar dumb-jump-find-rules '((:type "function" :language "elisp" :regex "\\(defun\s+JJJ\s+")
+                               (:type "variable" :language "elisp" :regex "\\(defvar\s+JJJ\s+")
+                               (:type "variable" :language "elisp" :regex "\\(setq\s+JJJ\s+")))
+
+(defvar dumb-jump-language-modes '((:language "elisp" :mode "emacs-lisp-mode")))
+
+;; TODO: find project route
+;; TODO: find and combine and create all match rules joinning with -e
+;; (defun dj--get-command (query)
+;;   (s-concat dj-grep-prefix " grep " dj-grep-args
+
+;; TODO: process response
+(shell-command-to-string "grep -REn -e '\\(defun\s+' -e 'defvar ' .")
+
+;; (defun dump-jump-generate-command (mode)
+;;   (let* ((rules (dump-jump-get-rules-by-mode mode)
+
+
+(defun dumb-jump-get-rules-by-languages (languages)
+  "Get a list of rules with a list of languages"
+  (-mapcat (lambda (lang) (dumb-jump-get-rules-by-language lang)) languages))
+
+(defun dumb-jump-get-rules-by-mode (mode)
+  "Get a list of rules by a major mode"
+  (dumb-jump-get-rules-by-languages (dumb-jump-get-languages-by-mode mode)))
+
+(defun dumb-jump-get-rules-by-language (language)
+  "Get list of rules for a language"
+  (-filter (lambda (x) (string= (plist-get x ':language) language)) dumb-jump-find-rules))
+
+(defun dumb-jump-get-modes-by-language (language)
+  "Get all modes connected to a language"
+  (-map (lambda (x) (plist-get x ':mode))
+        (-filter (lambda (x) (string= (plist-get x ':language) language)) dumb-jump-language-modes)))
+
+(defun dumb-jump-get-languages-by-mode (mode)
+  "Get all languages connected to a mode"
+  (-map (lambda (x) (plist-get x ':language))
+        (-filter (lambda (x) (string= (plist-get x ':mode) mode)) dumb-jump-language-modes)))
+
+(length '(1 2 3 4))
+
+;; for parsing a grep line
+;;(-map (lambda (x) (s-split ":" x)) (s-split "\n" "a:1\nb:2\nc:c3"))
 
 
 (provide 'dumb-jump)
--- test/dumb-jump-test.el
+++ test/dumb-jump-test.el
@@ -2,11 +2,21 @@
 (require 'dumb-jump)
 (require 'ert)
 (require 'f)
+(require 'dash)
 
 (setq test-data-dir (f-expand "./test/data"))
 
 (ert-deftest dumb-jump-dummy-test ()
   (should (string= (dumb-jump-asdf) "asdf")))
+
+(ert-deftest dumb-jump-mode-to-language-test ()
+  (should (-contains? (dumb-jump-get-languages-by-mode "emacs-lisp-mode") "elisp")))
+
+(ert-deftest dumb-jump-language-to-mode-test ()
+  (should (-contains? (dumb-jump-get-modes-by-language "elisp") "emacs-lisp-mode")))
+
+(ert-deftest dumb-jump-get-rules-by-mode-test ()
+  (should (= 3 (length (dumb-jump-get-rules-by-mode "emacs-lisp-mode")))))
 
 (ert-deftest data-dir-exists-test ()
   (should (f-dir? test-data-dir)))
