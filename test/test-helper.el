fbe270c351e9438e2cdd52f7daaf0a98ed2a3f95
Jack <jacktasia@gmail.com>
2016-01-07 13:16:45 -0800

    change path to dumb-jump for undercover

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,6 +1,6 @@
 
-
-(require 'ert)
+(require 'f)
 (require 'undercover)
-(undercover "dump-jump.el")
+(defvar dumb-jump-file (f-join (f-parent (f-parent (f-this-file))) "dump-jump.el"))
+(undercover dumb-jump-file)
 (require 'dumb-jump)
