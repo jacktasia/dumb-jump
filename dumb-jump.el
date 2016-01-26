b2c6053194949e1240bd3dfc367f1da1d0d092e0
Jack <jacktasia@gmail.com>
2016-01-26 14:28:20 -0800

    remove markdown from commentary

--- dumb-jump.el
+++ dumb-jump.el
@@ -7,7 +7,7 @@
 ;; Keywords: programming
 ;;; Commentary:
 
-;; **Dumb Jump** is an Emacs "jump to definition" package with support for multiple programming languages that favors "just working" over speed or accuracy.  This means minimal -- and ideally zero -- configuration with absolutely no stored indexes (TAGS) or persistent background processes.
+;; Dumb Jump is an Emacs "jump to definition" package with support for multiple programming languages that favors "just working" over speed or accuracy.  This means minimal -- and ideally zero -- configuration with absolutely no stored indexes (TAGS) or persistent background processes.
 
 ;;; Code:
 (require 'org)
