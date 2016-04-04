87a3516340e7734e91f83b3046379a66818c3e21
Jack <jacktasia@gmail.com>
2016-04-03 21:23:55 -0700

    update about commentary

--- dumb-jump.el
+++ dumb-jump.el
@@ -1,4 +1,4 @@
-;;; dumb-jump.el --- jump to variable and function definitions with no configuration
+;;; dumb-jump.el --- jump to definition for multiple languages without configuration.
 
 ;; Copyright (C) 2015-2016 jack angers
 ;; Author: jack angers
@@ -10,7 +10,8 @@
 
 ;; Dumb Jump is an Emacs "jump to definition" package with support for multiple programming languages that favors
 ;; "just working" over speed or accuracy.  This means minimal -- and ideally zero -- configuration with absolutely
-;; no stored indexes (TAGS) or persistent background processes.
+;; no stored indexes (TAGS) or persistent background processes. Dumb Jump performs best with The Silver Searcher
+;; `ag` installed.
 
 ;;; Code:
 (require 'f)
