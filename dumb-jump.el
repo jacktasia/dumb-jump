733be0acee56f9e179ef14d6ee8356bd67af40a0
jack <jacktasia@gmail.com>
2016-05-26 15:02:55 -0700

    set emacs 24.4 as min version, closes #26

--- dumb-jump.el
+++ dumb-jump.el
@@ -3,15 +3,15 @@
 ;; Copyright (C) 2015-2016 jack angers
 ;; Author: jack angers
 ;; Version: 0.3.8
-;; Package-Requires: ((f "0.17.3") (s "1.11.0") (dash "2.9.0") (popup "0.5.3"))
+;; Package-Requires: ((emacs "24.4") (f "0.17.3") (s "1.11.0") (dash "2.9.0") (popup "0.5.3"))
 ;; Keywords: programming
 
 ;;; Commentary:
 
 ;; Dumb Jump is an Emacs "jump to definition" package with support for multiple programming languages that favors
 ;; "just working" over speed or accuracy.  This means minimal -- and ideally zero -- configuration with absolutely
-;; no stored indexes (TAGS) or persistent background processes. Dumb Jump performs best with The Silver Searcher
-;; `ag` installed.
+;; no stored indexes (TAGS) or persistent background processes.  Dumb Jump performs best with The Silver Searcher
+;; `ag` installed.  Dumb Jump requires at least GNU Emacs 24.4.
 
 ;;; Code:
 (require 'f)
