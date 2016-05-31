9209939a2177feb9a5966fbe21537b7c53d6d6e9
jack <jacktasia@gmail.com>
2016-05-31 12:43:35 -0700

    hopefull fix for some greps not supporting non-greedy

--- dumb-jump.el
+++ dumb-jump.el
@@ -141,7 +141,7 @@
 
     ;; python
     (:type "variable" :language "python"
-           :regex "\\s*JJJ\\s*=[^=]+?" :tests ("test = 1234") :not ("if test == 1234:"))
+           :regex "\\s*JJJ\\s*=[^=\\n]+" :tests ("test = 1234") :not ("if test == 1234:"))
 
     (:type "function" :language "python"
            :regex "def\\s*JJJ\\s*\\\("
@@ -153,7 +153,7 @@
 
     ;; ruby
     (:type "variable" :language "ruby"
-           :regex "\\s*JJJ\\s*=[^=]+?" :tests ("test = 1234") :not ("if test == 1234"))
+           :regex "\\s*JJJ\\s*=[^=\\n]+" :tests ("test = 1234") :not ("if test == 1234"))
 
     (:type "function" :language "ruby"
            :regex "\\bdef\\s*JJJ\\s*\\\("
@@ -183,7 +183,7 @@
 
     ;; go
     (:type "variable" :language "go"
-           :regex "\\s*\\bJJJ\\s*=[^=]+?" :tests ("test = 1234") :not ("if test == 1234 {"))
+           :regex "\\s*\\bJJJ\\s*=[^=\\n]+" :tests ("test = 1234") :not ("if test == 1234 {"))
 
     (:type "variable" :language "go"
            :regex "\\s*\\bJJJ\\s*:=\\s*" :tests ("test := 1234"))
@@ -223,7 +223,7 @@
 
     ;; javascript
     (:type "variable" :language "javascript"
-           :regex "\\s*\\bJJJ\\s*=[^=]+?" :tests ("test = 1234" "const test = props =>") :not ("if (test === 1234)"))
+           :regex "\\s*\\bJJJ\\s*=[^=\\n]+" :tests ("test = 1234" "const test = props =>") :not ("if (test === 1234)"))
 
     (:type "variable" :language "javascript"
            :regex "\\bfunction\\b[^\\(]*\\\(\\s*[^\\)]*\\bJJJ\\b\\s*,?\\s*\\\)?"
@@ -245,7 +245,7 @@
 
     ;; lua
     (:type "variable" :language "lua"
-           :regex "\\s*\\bJJJ\\s*=[^=]+?" :tests ("test = 1234") :not ("if test === 1234"))
+           :regex "\\s*\\bJJJ\\s*=[^=\\n]+" :tests ("test = 1234") :not ("if test === 1234"))
 
     (:type "variable" :language "lua"
            :regex "\\bfunction\\b[^\\(]*\\\(\\s*[^\\)]*\\bJJJ\\b\\s*,?\\s*\\\)?"
