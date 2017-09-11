c98737289a349ae8ae25c6df19bbd29ae6c146af
Andrey Shedko <ayshedko@yandex.ru>
2017-09-11 19:37:56 +0300

    JLSupport : Added support for {T} tmpl

--- dumb-jump.el
+++ dumb-jump.el
@@ -612,11 +612,11 @@ or most optimal searcher."
 
     ;; julia
     (:type "function" :supports ("ag" "grep" "rg" "git-grep") :language "julia"
-           :regex "(@noinline|@inline)?\\s*function\\s*JJJ\\("
+           :regex "(@noinline|@inline)?\\s*function\\s*JJJ\\{[^\\}]*\\}\\("
            :tests ("function test()" "@inline function test()"))
 
     (:type "function" :supports ("ag" "grep" "rg" "git-grep") :language "julia"
-           :regex "(@noinline|@inline)?JJJ\\([^\\)]*\\)\s*="
+           :regex "(@noinline|@inline)?JJJ\\{[^\\}]*\\}\\([^\\)]*\\)\s*="
            :tests ("test(a)=1" "test(a,b)=1*8" "@noinline test()=1"))
 
     (:type "function" :supports ("ag" "grep" "rg" "git-grep") :language "julia"
