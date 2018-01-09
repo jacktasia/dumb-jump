a2bcfd357e16b3404cd0e210fedbc9a584da83b8
Morten Kristensen <msk@nullpointer.dk>
2018-01-09 22:18:34 +0100

    Extended SML support (#198)
    * Support SML structure, signature, and functor as modules
    
    * Group type and datatype
    
    * Match variable sigs
    
    * Match functions broader

--- dumb-jump.el
+++ dumb-jump.el
@@ -994,20 +994,37 @@ or most optimal searcher."
 
     ;; sml
     (:type "type" :supports ("ag" "grep" "rg" "git-grep") :language "sml"
-           :regex "\\s*(datatype)\\s+\\bJJJ\\b\\s*="
+           :regex "\\s*(data)?type\\s+.*\\bJJJ\\b"
            :tests ("datatype test ="
-                   "datatype test=")
+                   "datatype test="
+                   "datatype 'a test ="
+                   "type test ="
+                   "type 'a test ="
+                   "type 'a test"
+                   "type test")
            :not ("datatypetest ="))
 
     (:type "variable" :supports ("ag" "grep" "rg" "git-grep") :language "sml"
-           :regex "\\s*val\\s+\\bJJJ\\b\\s*="
+           :regex "\\s*val\\s+\\bJJJ\\b"
            :tests ("val test ="
-                   "val test="))
+                   "val test="
+                   "val test : bool"))
 
     (:type "function" :supports ("ag" "grep" "rg" "git-grep") :language "sml"
-           :regex "\\s*fun\\s+\\bJJJ\\b\\s*(\\w+|\\((\\s*\\w\\s*,?)+\\))\\s*="
+           :regex "\\s*fun\\s+\\bJJJ\\b.*\\s*="
            :tests ("fun test list ="
-                   "fun test (STRING_NIL, a) =")))
+                   "fun test (STRING_NIL, a) ="
+                   "fun test ((s1,s2): 'a queue) : 'a * 'a queue ="
+                   "fun test (var : q) : int ="
+                   "fun test f e xs ="))
+
+    (:type "module" :supports ("ag" "grep" "rg" "git-grep") :language "sml"
+           :regex "\\s*(structure|signature|functor)\\s+\\bJJJ\\b"
+           :tests ("structure test ="
+                   "structure test : MYTEST ="
+                   "signature test ="
+                   "functor test (T:TEST) ="
+                   "functor test(T:TEST) =")))
 
   "List of regex patttern templates organized by language and type to use for generating the grep command."
   :group 'dumb-jump
