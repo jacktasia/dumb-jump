94dc1945dd04375e5561684a054c4ab5380162e9
USAMI Kenta <tadsan@zonu.me>
2018-08-18 17:17:06 +0900

    Add PHPDoc's @method and @property tag for PHP magic method (#227)

--- dumb-jump.el
+++ dumb-jump.el
@@ -616,10 +616,17 @@ or most optimal searcher."
            :regex "function\\s*JJJ\\s*\\\("
            :tests ("function test()" "function test ()"))
 
+    (:type "function" :supports ("ag" "grep" "rg" "git-grep") :language "php"
+           :regex "\\*\\s@method\\s+[^ 	]+\\s+JJJ\\("
+           :tests ("/** @method string|false test($a)" " * @method bool test()"))
+
     (:type "variable" :supports ("ag" "grep" "rg" "git-grep") :language "php"
            :regex "(\\s|->|\\$|::)JJJ\\s*=\\s*"
            :tests ("$test = 1234" "$foo->test = 1234"))
 
+    (:type "variable" :supports ("ag" "grep" "rg" "git-grep") :language "php"
+           :regex "\\*\\s@property(-read|-write)?\\s+([^ 	]+\\s+)&?\\$JJJ(\\s+|$)"
+           :tests ("/** @property string $test" "/** @property string $test description for $test property"  " * @property-read bool|bool $test" " * @property-write \\ArrayObject<string,resource[]> $test"))
     (:type "trait" :supports ("ag" "grep" "rg" "git-grep") :language "php"
            :regex "trait\\s*JJJ\\s*\\\{"
            :tests ("trait test{" "trait test {"))
