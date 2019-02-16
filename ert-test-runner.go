9076523969db418297f968bb8e5970ed4d26290e
jack <jacktasia@gmail.com>
2019-02-16 10:17:39 -0800

    saving

--- ert-test-runner.go
+++ ert-test-runner.go
@@ -3,16 +3,22 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	//	"os"
 	"os/exec"
 	"regexp"
+	"strconv"
 	"strings"
 	"time"
 )
 
 // TODO: this should run inside of a docker container with all the dependencies set!
-// TODO: take file argument and list of `--slow` patterns
+// TODO: take file argument and list of `--serialize` and `--slow` patterns
 // TODO: make helper that will download from this repo as raw and and run `go run ert-test-runner.go my-tests.el --slow '.*-ag-.*'
+// TODO: add tests for the test runner
+
+func maintest() {
+	s := "Ran 19 tests, 19 results as expected"
+	fmt.Println(extractRunCount(s))
+}
 func main() {
 
 	fmt.Println("ert-test-runner!")
@@ -31,9 +37,7 @@ func main() {
 	//standalones := []string{"dumb-jump-test-ag-rules-test"}
 	standalones := []string{}
 
-	groups := []string{".*-ag-.*", ".*-rg-.*", "dumb.*-git-grep.*"} // ".*-git-grep-plus-ag.*", ".*-git-grep.*",
-	// "dumb-jump-shell-command-switch-.*", "dumb-jump-pick-grep-variant-.*", "dumb-jump-handle-results-.*", "dumb-jump-message-.*", "dumb-jump-generate-.*", "dumb-jump-go-.*", "dumb-jump-react-.*", "dumb-jump-generators-.*", "dumb-jump-get-lang-.*", "dumb-jump-test-grep-.*", "dumb-jump-run-.*", "dumb-jump-prompt-.*"}
-	//groupCompiles := []*regexp.Regexp{}
+	groups := []string{".*-ag-.*", ".*-rg-.*"}
 	r, err := regexp.Compile("ert-deftest\\s([^\\s]+)\\s")
 
 	matches := r.FindAllStringSubmatch(contents, -1)
@@ -43,7 +47,8 @@ func main() {
 		defs = append(defs, match[1])
 	}
 
-	// TODO:
+	totalTests := len(defs)
+
 	remainders := []string{}
 	remainders = append(remainders, defs...)
 	for _, standalone := range standalones {
@@ -52,49 +57,38 @@ func main() {
 	testGroups := [][]string{}
 	var testGroup []string
 	for _, group := range groups {
-		//regexp.MatchString(group,
 		remainders, testGroup = takeMatches(remainders, group)
-
+		fmt.Println("Removed", testGroup)
 		testGroups = append(testGroups, testGroup)
 	}
-
-	// for _, ertTest := range remainders {
-	// 	//fmt.Println("Running " + ertTest)
-	// 	runErtTest(ertTest)
-	// }
 
 	fmt.Println("defs len", len(defs))
 	fmt.Println("remainders len", len(remainders))
 	fmt.Println("test groups len", len(testGroups))
 
 	fmt.Println("------------")
-	fmt.Println(remainders)
+	//fmt.Println(remainders)
 	fmt.Println("Running....")
-
-	//runErtTestsConcurrently(remainders)
 
 	toRun := []string{}
 	toRun = append(toRun, groups...)
-	//toRun = append(toRun, remainders...)
 
 	chunked := chunk(remainders, 30)
-
 	for _, c := range chunked {
 		toRun = append(toRun, makePattern(c))
 	}
 
-	//chunkSize := (len(logs) + numCPU - 1) / numCPU
-
+	// start running
 	start := time.Now()
-
+	totalRan := 0
 	for _, standalone := range standalones {
-		runErtTest(standalone)
+		totalRan += extractRunCount(runErtTest(standalone))
 	}
 
-	runErtTestsConcurrently(toRun)
+	totalRan += runErtTestsConcurrently(toRun)
 	end := time.Now()
 	fmt.Println("------------")
-	fmt.Println("Done. took", end.Sub(start))
+	fmt.Println("Done. Ran", totalRan, "tests of", totalTests, " and took", end.Sub(start))
 }
 
 func makePattern(items []string) string {
@@ -127,7 +121,6 @@ func worker(id int, jobs <-chan string, results chan<- string) {
 		result := runErtTest(string(j))
 		end := time.Now()
 		fmt.Println(j[0:5], "size", len(j), "took", end.Sub(start))
-		//result := "2"
 		// fmt.Println("worker", id, "finished job", j)
 		results <- result
 	}
@@ -149,31 +142,60 @@ func chunk(logs []string, chunkSize int) [][]string {
 	return divided
 }
 
-func runErtTestsConcurrently(ertTests []string) {
+func runErtTestsConcurrently(ertTests []string) int {
 	testCount := len(ertTests)
 
 	jobs := make(chan string, testCount)
 	results := make(chan string, testCount)
 
+	// TODO: base off cpu count?
+	// spin up 6 workers
 	workerCount := 6
 	for w := 1; w <= workerCount; w++ {
 		go worker(w, jobs, results)
 	}
 
-	// Here we send 5 `jobs` and then `close` that
+	// send the `jobs` and `close` that
 	// channel to indicate that's all the work we have.
 	for j := 0; j < testCount; j++ {
 		jobs <- ertTests[j]
 	}
 	close(jobs)
 
-	// Finally we collect all the results of the work.
-	// for a := 1; a <= 5; a++ {
 	fmt.Println("Waiting....")
+
+	// collect results
+	// TODO: gather list of tests
+	totalRan := 0
 	for a := 1; a <= testCount; a++ {
-		fmt.Println("Done with")
-		<-results
+		totalRan += extractRunCount(<-results)
 	}
+
+	return totalRan
+}
+
+// TODO: also extract the list of tests themselves so we can figure out exactly which ones were dropped
+func extractRunCount(output string) int {
+	r, err := regexp.Compile("Ran ([0-9]+) tests, ([0-9]+) results as expected")
+	if err != nil {
+		panic(err)
+	}
+	// "Ran 19 tests, 19 results as expected"
+
+	rawMatches := r.FindAllStringSubmatch(output, -1)
+
+	if len(rawMatches) != 1 {
+		panic("no len match")
+	}
+
+	matches := rawMatches[0]
+	fmt.Println(matches)
+	if matches[1] != matches[2] {
+		panic(matches[1] + "does not equal" + matches[2])
+	}
+
+	i, _ := strconv.Atoi(string(matches[1]))
+	return i
 }
 
 func runErtTest(testName string) string {
@@ -187,8 +209,6 @@ func runErtTest(testName string) string {
 		fmt.Println("~~~~ ERRROR", testName, err, "--", string(out))
 
 	}
-
-	// fmt.Println(string(out))
 
 	return string(out)
 }
