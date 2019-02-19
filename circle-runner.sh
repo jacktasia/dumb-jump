a06d7819db16bbab8ec786a5f48f58b180d8aa03
jack <jacktasia@gmail.com>
2019-02-18 23:20:15 -0800

    add circle ci runner

---
+++ circle-runner.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source /home/travis/.bashrc
+source /home/travis/.bash_profile
+sudo chmod 777 -R .
+echo "installing $EVM_EMACS"
+#evm uninstall emacs-24.3-travis
+evm install $EVM_EMACS --use --skip
+emacs --version
+ls
+cask
+make test
+make test-concurrent
