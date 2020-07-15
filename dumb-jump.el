b4573e16efec5363fc65d6294d2e56fe005dc1be
Philip K <philip@warpmail.net>
2020-07-15 19:28:26 +0200

    Updated Commentary section to explain xref

--- dumb-jump.el
+++ dumb-jump.el
@@ -21,10 +21,22 @@
 
 ;;; Commentary:
 
-;; Dumb Jump is an Emacs "jump to definition" package with support for 40+ programming languages that favors
-;; "just working" over speed or accuracy.  This means minimal -- and ideally zero -- configuration with absolutely
-;; no stored indexes (TAGS) or persistent background processes.  Dumb Jump performs best with The Silver Searcher
-;; `ag` or ripgrep `rg` installed.  Dumb Jump requires at least GNU Emacs 24.3.
+;; Dumb Jump provides a xref-based interface for jumping to
+;; definitions. It is based on tools such as grep, the silver searcher
+;; (https://geoff.greer.fm/ag/), ripgrep
+;; (https://github.com/BurntSushi/ripgrep) or git-grep
+;; (https://git-scm.com/docs/git-grep). This means minimal -- and
+;; ideally zero -- configuration with absolutely no stored indexes
+;; (TAGS) or persistent background processes.
+;;
+;; To enable Dumb Jump, add the following to your initialisation file:
+;;
+;;    (add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
+;;
+;; Now pressing M-. on an identifier should open a buffer at the place
+;; where it is defined, or a list of candidates if uncertain. This
+;; list can be navigated using M-g M-n (next-error) and M-g M-p
+;; (previous-error).
 
 ;;; Code:
 (unless (require 'xref nil :noerror)
