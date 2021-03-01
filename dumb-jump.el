b15fa2f25f563700824229beef50865db48750b3
Magnus Nyberg <magnus@nexter.se>
2021-03-01 10:31:24 +0100

    Update commentary

--- dumb-jump.el
+++ dumb-jump.el
@@ -21,7 +21,7 @@
 
 ;;; Commentary:
 
-;; Dumb Jump is an Emacs "jump to definition" package with support for 40+ programming languages that favors
+;; Dumb Jump is an Emacs "jump to definition" package with support for 50+ programming languages that favors
 ;; "just working" over speed or accuracy.  This means minimal -- and ideally zero -- configuration with absolutely
 ;; no stored indexes (TAGS) or persistent background processes.
 ;;
