d537d8257bc43582bcb4a704b2b847da8169361c
Precise Simulation <18078865+precise-simulation@users.noreply.github.com>
2022-05-24 10:18:04 +0200

    added project root denoter for Fossil SCM

--- dumb-jump.el
+++ dumb-jump.el
@@ -1729,7 +1729,7 @@ a symbol then it's probably a function call"
                                (string :tag "Regular expression")))))))
 
 (defcustom dumb-jump-project-denoters
-  '(".dumbjump" ".projectile" ".git" ".hg" ".fslckout" ".bzr" "_darcs" ".svn" "Makefile" "PkgInfo" "-pkg.el")
+  '(".dumbjump" ".projectile" ".git" ".hg" ".fslckout" ".bzr" "_darcs" ".svn" "Makefile" "PkgInfo" "-pkg.el" "_FOSSIL_")
   "Files and directories that signify a directory is a project root."
   :group 'dumb-jump
   :type '(repeat (string  :tag "Name")))
