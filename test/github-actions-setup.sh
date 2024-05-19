570b095b60ebdbc0bddf2f06c4af26511229e874
Ryan Davis <ryand-ruby@zenspider.com>
2024-05-18 23:56:04 -0700

    CI revamp: bump emacs, bump ripgrep, and trim dead stuff
    * Update ripgrep
    * Remove ag python and ruby from workflow
    * Add newer emacsen versions
    * Why is there python?

--- test/github-actions-setup.sh
+++ test/github-actions-setup.sh
@@ -1,11 +1,5 @@
+export RG_VER=14.1.0
 
 sudo apt-get -qq update || ls
-sudo apt-get install -y --no-install-recommends software-properties-common curl
-sudo apt-get -qq update || ls
-sudo apt-get install -y --no-install-recommends silversearcher-ag git python ruby
-sudo curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb && sudo dpkg -i ripgrep_0.10.0_amd64.deb
-#ENV PATH="/root/.evm/bin:$PATH"
-#ENV PATH="/root/.cask/bin:$PATH"
-
-
-sudo apt-get install -y --no-install-recommends make libgtk2.0-0 libsm-dev
+sudo apt-get install -y --no-install-recommends software-properties-common curl git make
+sudo curl -LO https://github.com/BurntSushi/ripgrep/releases/download/${RG_VER}/ripgrep_${RG_VER}-1_amd64.deb && sudo dpkg -i ripgrep_${RG_VER}-1_amd64.deb
