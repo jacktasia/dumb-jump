ef348bd75035fd9157b62c59920ad8c9bee50a94
Jack Angers <jacktasia@gmail.com>
2026-01-27 17:42:21 -0800

    drop undercover from test-helper

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,10 +1,5 @@
 
 (require 'ert)
-(require 'undercover)
-(undercover "*.el"
-            (:exclude "*-test.el")
-            (:send-report t)
-            (:report-file "/tmp/undercover-report.json"))
 (require 'dumb-jump)
 
 (require 'el-mock)
