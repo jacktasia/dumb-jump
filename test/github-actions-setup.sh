08e84a20ac537a119ada6b65e3c3c42735457593
Jack <jacktasia@gmail.com>
2026-02-28 11:25:55 -0800

    bump rg test version

--- test/github-actions-setup.sh
+++ test/github-actions-setup.sh
@@ -1,11 +1,9 @@
-
 sudo apt-get -qq update || ls
 sudo apt-get install -y --no-install-recommends software-properties-common curl
 sudo apt-get -qq update || ls
 sudo apt-get install -y --no-install-recommends silversearcher-ag git python ruby
-sudo curl -LO https://github.com/BurntSushi/ripgrep/releases/download/0.10.0/ripgrep_0.10.0_amd64.deb && sudo dpkg -i ripgrep_0.10.0_amd64.deb
+sudo curl -LO https://github.com/BurntSushi/ripgrep/releases/download/14.1.0/ripgrep_14.1.0_amd64.deb && sudo dpkg -i ripgrep_14.1.0_amd64.deb
 #ENV PATH="/root/.evm/bin:$PATH"
 #ENV PATH="/root/.cask/bin:$PATH"
-
 
 sudo apt-get install -y --no-install-recommends make libgtk2.0-0 libsm-dev
